4ab41688df696656a26972d50b34e5f4b092bd76
CalebeMar <calebe.martinsalmeida@gmail.com>
2026-08-10 17:11:12 -0300

    arrumando coisas dos tofs e dos motores no main

--- Teensy_code/lib/ToFCalibration/ToFCalibration.cpp
+++ Teensy_code/lib/ToFCalibration/ToFCalibration.cpp
@@ -16,7 +16,7 @@ struct SideConfig {
 static const SideConfig LADOS[] = {
   { 0, 4, {0, 1, 2, 3} },   
   { 1, 4, {4, 5, 6, 7} },   
-  { 2, 4, {8, 9, 10, 11} }  
+  { 2, 3, {8, 9, 10} }  
 };
 constexpr uint8_t NUM_LADOS = sizeof(LADOS) / sizeof(LADOS[0]);
 
